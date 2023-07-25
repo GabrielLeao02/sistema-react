66195793f763c67ce4b47eef5822bd3b72e7dddb
Gabriel Leao <gabriel.edg210@gmail.com>
2023-07-25 09:56:12 -0300

    adicionado  propriedades de estilo ao home

--- .history/src/presenter/containerGlobal/containerStyled_20230725092343.tsx
+++ .history/src/presenter/containerGlobal/containerStyled_20230725092343.tsx
@@ -9,12 +9,11 @@ const ContainerStyled = styled.div`
   margin-top: 80px;
   width: 80%;  
   max-width: 800px;
-  background: rgba( 255, 255, 255, 0.35 );
-  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-  backdrop-filter: blur( 13.5px );
+  background: rgba( 255, 255, 255, 0.75 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 ); 
   -webkit-backdrop-filter: blur( 13.5px );
   border-radius: 10px;
-  border: 1px solid rgba( 255, 255, 255, 0.18 );
+  border: 1px solid rgba( 255, 255, 255, 0.18 ); 
   padding: 20px;
 `;
 export default ContainerStyled
--- .history/src/presenter/containerGlobal/containerStyled_20230725094304.tsx
+++ .history/src/presenter/containerGlobal/containerStyled_20230725094304.tsx
@@ -9,7 +9,7 @@ const ContainerStyled = styled.div`
   margin-top: 80px;
   width: 80%;  
   max-width: 800px;
-  background: rgba( 255, 255, 255, 0.35 );
+  background: rgba( 255, 255, 255, 0.75 );
   box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
   backdrop-filter: blur( 13.5px );
   -webkit-backdrop-filter: blur( 13.5px );
