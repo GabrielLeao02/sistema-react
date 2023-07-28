032a298f76960c7f69fda8fe050698a55369fd1e
Gabriel Leao <gabriel.edg210@gmail.com>
2023-07-28 17:12:44 -0300

    refactor: Translate code and update component names to English
    - Translated all variable names, labels, and error messages to English for better readability.
    - Renamed the component "FormLogin" to "LoginForm" for consistency.
    - Updated the variable names "usuarioEmailError" and "errorLogin" to "userEmailError" and "errorLogin" respectively.
    - Replaced the function name "handleLoginUser" with "handleLoginUser".
    - Changed the function name "handleSubmit" to "handleSubmit".
    - Updated comments and variable names in the "FormLogin" component.
    - Renamed the function "setMostrarBotao" to "setShowButton" in the "FormLoginProps" type.
    - Replaced "usuario" with "user" for variable names related to user data.
    - Updated the "FormStyled" component and related styles.
    
    These changes improve code readability and maintain consistency across the application.

---
+++ src/presenter/containerGlobal/GlobalContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const GlobalContainer = styled.div`
+	padding: 20px;
+	box-sizing: border-box;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+`;
+
+export default GlobalContainer;
